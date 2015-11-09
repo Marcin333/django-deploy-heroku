15426a33874f2ab6704eb47172924f6094203877
Marcin <marcinryszard80@gmail.com>
2015-11-09 14:17:39 +0100

    procfile

--- ecommerce2/wsgi.py
+++ ecommerce2/wsgi.py
@@ -8,13 +8,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce2.settings")
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
 
 try:
 	from dj_static import Cling
