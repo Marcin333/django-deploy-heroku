33b756d6f96735210145dde1f8947e0577b3adf6
Marcin <marcinryszard80@gmail.com>
2015-11-11 09:50:57 +0100

    settings wsgi edit

--- ecommerce2/settings/settings.py
+++ ecommerce2/settings/settings.py
@@ -24,30 +24,5 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR), 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'newsletter.context_processors.condition_all',
-            ],
-        },
-    },
-]
 
-
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'static_root')
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static', 'media')
-
-# STATICFILES_DIRS are gonna to go to the STATIC_ROOT after collectstatic
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static', 'static_files'),)
 
