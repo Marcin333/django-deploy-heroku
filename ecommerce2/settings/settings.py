9b765942ad38b7f31aecf4299b5fa2685ba389d2
Marcin <marcinryszard80@gmail.com>
2015-11-11 10:08:43 +0100

    settings wsgi edit

--- ecommerce2/settings/settings.py
+++ ecommerce2/settings/settings.py
@@ -1,10 +1,6 @@
 import os
 from django.conf import settings
 
-# PYTHONPATH_SETTINGS = 'ecommerce2.settings'
-# # specify the name of your settings module
-# os.environ['DJANGO_SETTINGS_MODULE'] = PYTHONPATH_SETTINGS
-
 DATABASES = settings.DATABASES
 
 # Parse database configuration from $DATABASE_URL
@@ -22,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = False
 SECRET_KEY = os.environ['SECRET_KEY'] 
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
